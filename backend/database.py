04b6f54e37ad48a4627b467f5e436829b6b286dd
Balram Mandal <rkrm0209@gmail.com>
2026-04-10 16:32:28 +0530

    Updated database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "sqlite:///./database.db"
 
@@ -8,6 +7,7 @@ engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(DeclarativeBase):
+    pass
